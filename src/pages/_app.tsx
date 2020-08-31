d6262c4129e4d82d60455834e512218ff96a9b9b
Jose Ross Barredo <iamjoross@gmail.com>
2020-08-31 11:07:03 +0800

    Add Errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,24 @@
 import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core';
-
+import { createClient, Provider } from 'urql';
 import theme from '../theme';
 
-function MyApp({ Component, pageProps }) {
+const client = createClient({
+  url: 'http://dev.test-site.com:4000/graphql',
+  fetchOptions: {
+    credentials: 'include',
+  },
+});
+
+function MyApp({ Component, pageProps }: any) {
   return (
-    <ThemeProvider theme={theme}>
-      <ColorModeProvider value='light'>
-        <CSSReset />
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ThemeProvider>
+    <Provider value={client}>
+      <ThemeProvider theme={theme}>
+        <ColorModeProvider value='light'>
+          <CSSReset />
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
